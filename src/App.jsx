5987fb5eeb8d05f0141c08f7b6702c148aa80d48
cf2-exe <mehdibichami@gmail.com>
2025-03-08 15:41:06 +0100

    w

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
     <>
      <Router>
         <Routes>
-          <Route path="/"  element={<Home />} />
+          <Route path="/Weblocatores/"  element={<Home />} />
         </Routes>
       </Router>
     </>
